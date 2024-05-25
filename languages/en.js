32513356b19ec42360d3222188dee220270cf295
ClaudiaTLD <toledocclaudia@gmail.com>
2024-05-24 23:54:10 -0300

    first commit

---
+++ languages/en.js
@@ -0,0 +1,26 @@
+{
+    "profile"; {
+        "rol"; "Front-end developer",
+        "description"; "My journey as web developer started learning the basics of HTML and CSS. Then I learned to create more complex designs, working with JavaScript and frameworks to develop dynamic web pages and create visual effects. I enjoy my work and enjoy working as a team to meet the needs of clients and users."
+    };
+    "experience";{
+        "first-job-title"; "Front-end developer",
+        "first-job-dates"; "February 2017",
+        "first-job-description"; "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+    };
+    "projects";{
+        "first-project-title"; "Hotel",
+        "first-project-dates"; "May 2017",
+        "first-project-description"; "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+
+        
+        "second-project-title"; "Landing page",
+        "second-project-dates"; "June 2017",
+        "second-project-description"; "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+
+            
+        "last-project-title"; "Portfolio",
+        "last-project-dates"; "April 2024",
+        "last-project-description"; "Lorem ipsum, dolor sit amet consectetur adipisicing elit."    
+}
+}
---
+++ languages/es.js
@@ -0,0 +1,26 @@
+{
+    "profile"; {
+        "rol"; "Desarrollador Front-end",
+        "description"; "Mi viaje con el desarrollo web comenzó aprendiendo los conceptos básicos de HTML y CSS. Luego aprendí a crear diseños más complejos, trabajando con JavaScript y frameworks para desarrollar páginas web dinámicas y crear efectos visuales. Disfruto de mi trabajo y así como del trabajo en equipo para satisfacer las necesidades de clientes y usuarios."
+    };
+    "experience";{
+        "first-job-title"; "Desarrollador Front-end",
+        "first-job-dates"; "Febrero 2017",
+        "first-job-description"; "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+    };
+    "projects";{
+        "first-project-title"; "Hotel",
+        "first-project-dates"; "Mayo 2017",
+        "first-project-description"; "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+
+        
+        "second-project-title"; "Landing Page",
+        "second-project-dates"; "Junio 2017",
+        "second-project-description"; "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+
+            
+        "last-project-title"; "Portfolio",
+        "last-project-dates"; "Abril 2024",
+        "last-project-description"; "Lorem ipsum, dolor sit amet consectetur adipisicing elit."    
+}
+}
